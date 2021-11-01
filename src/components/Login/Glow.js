4622f6455c1227a686ff08829f986fab25b263dc
mp4 <puleri.mp4@gmail.com>
2021-11-01 16:34:18 -0700

    Login button + glow added

---
+++ src/components/Login/Glow.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Login.css';
+
+
+export default function Glow() {
+    return (
+      <div className="glow-div">
+
+          <button className="homepage-link">Login</button>
+
+      </div>
+    )
+  }
+
+// <span></span>
+// <span></span>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import Glow from './Glow.js'
 
 export default function Login() {
   return (
     <div>
       Login
+      <Glow />
     </div>
   )
 }

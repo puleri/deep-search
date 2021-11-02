89f7adc1f2b3671a01617ad8deeb4516b099811a
mp4 <puleri.mp4@gmail.com>
2021-11-01 21:36:15 -0700

    debugging props

--- src/App.js
+++ src/App.js
@@ -4,18 +4,27 @@ import Login from "./components/Login/Login.js";
 import Astro from "./components/Astro/Astro.js";
 import Profile from "./components/Profile/Profile.js";
 import ISS from "./components/ISS/ISS.js";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import {firebase} from './firebase';
 
 export default function App() {
+
   const [isUserSignedIn, setIsUserSignedIn] = useState(false);
+  const [currentUser, setCurrentUser] = useState
+  ({
+    username: '',
+    email: ''
+  })
+
   firebase.auth().onAuthStateChanged((user) => {
     if(user){
+
       return setIsUserSignedIn(true);
     }
     setIsUserSignedIn(false);
   })
+
   if (isUserSignedIn) {
     return (
       <div className="App">
@@ -23,6 +32,7 @@ export default function App() {
               exact
               path="/"
               component={Profile}
+              currentUser={currentUser}
               />
 
 
@@ -44,6 +54,7 @@ export default function App() {
       <Route
       path="/"
       component={Login}
+      currentUser={currentUser}
       />
 
     )
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import { firebase } from '../../firebase';
 
-export default function Profile() {
+export default function Profile(props) {
+
 
   const signOut = () => {
     firebase.auth().signOut();
   }
-  
   return (
     <div>
       <button onClick={signOut}>Log out</button>
+      <button onClick={() => console.log(props)}>Props</button>
     </div>
   )
 }

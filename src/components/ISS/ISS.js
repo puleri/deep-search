121b0925446e45868e777ec3b2c52ad527463404
mp4 <puleri.mp4@gmail.com>
2021-11-02 09:50:42 -0700

    ISS API fetch added and loggable on ISS component

--- src/components/ISS/ISS.js
+++ src/components/ISS/ISS.js
@@ -1,8 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function ISS() {
+
+  const [data, setData] = useState({
+    message: 'pending',
+    timestamp: undefined,
+    iss_position: {},
+   })
+
+   useEffect(() => {
+     const url = "http://api.open-notify.org/iss-now.json";
+     const promise = fetch(url);
+     promise.then(res => {
+       return res.json();
+     })
+      .then(data => {
+        setData({
+          message: data.message,
+          timestamp: data.timestamp,
+          iss_position: data.iss_position
+        })
+        ;
+      })
+   }, [])
+
+   const issData = data.iss_position;
+
   return (
     <div>
+      <button onClick={()=> console.log(issData)}>Log</button>
       ISS
     </div>
   )

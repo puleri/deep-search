ead7169ef8ced0613e4ae52acefa2d0b232343f4
mp4 <puleri.mp4@gmail.com>
2021-11-02 21:06:42 -0700

    Nav styling and minor bug fixes

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,6 +4,7 @@ import './Profile.css'
 import { firebase } from '../../firebase';
 import astro from './astronaut.png';
 import rocket from './rocket.png';
+import iss from './iss.png';
 
 export default function Profile() {
 
@@ -29,17 +30,15 @@ export default function Profile() {
 
   return (
     <div className="profile-container">
-      <div className="navbar-container">
-        <img id="nav-astro" src={astro} alt="astronaut-icon" />
-        <img id="nav-rocket" src={rocket} alt="astronaut-icon" />
 
+      <div className="navbar-container">
+        <Link data-testid="Link" to="/astronauts"><img id="nav-astro" src={astro} alt="astronaut-icon" /></Link>
+        <Link data-testid="Link" to="/iss"><img id="nav-iss" src={iss} alt="iss-icon" /></Link>
+        <button onClick={signOut}><img id="nav-rocket" src={rocket} alt="astronaut-icon" /></button>
       </div>
+
       <div className="content-wrapper">
         <h1 id="welcome">Hi {firstName}</h1>
-
-        <Link data-testid="Link" to="/astronauts">Astronauts</Link>
-
-        <button onClick={signOut}>Log out</button>
       </div>
     </div>
   )

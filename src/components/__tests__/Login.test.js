d52a4015366b2a883f3036cc4b1ed931e4afd15e
mp4 <puleri.mp4@gmail.com>
2021-11-15 14:23:39 -0800

    refactored Login test suite to be DRYer

--- src/components/__tests__/Login.test.js
+++ src/components/__tests__/Login.test.js
@@ -1,38 +1,53 @@
 import React from 'react';
-import { render, screen, cleanup } from '@testing-library/react';
+import { render, screen, cleanup, fireEvent } from '@testing-library/react';
 import Login from '../Login/Login.js';
 import "@testing-library/jest-dom/extend-expect";
 
 afterEach(cleanup);
 
+let getByTestId;
+
+beforeEach(() => {
+  const component = render(<Login />)
+  getByTestId = component.getByTestId
+})
+
+
 test('should render Login component along with welcome message', () => {
-  render(<Login/>);
-  const loginElement = screen.getByTestId('Login');
+  const loginElement = getByTestId('Login');
   expect(loginElement).toBeInTheDocument();
   expect(loginElement).toHaveTextContent('Deep Search(Space edition)Login with Google')
 })
 
 test('should render login button', () => {
-  render(<Login/>);
-  const loginElement = screen.getByTestId('Button');
+  const loginElement = getByTestId('Button');
   expect(loginElement).toBeInTheDocument();
 })
 
 test("header renders with correct text", () => {
-  const { getByTestId } = render(<Login />);
   const headerEl = getByTestId('header');
 
   expect(headerEl.textContent).toBe("Deep Search");
 })
 
 test('subheader should reference videologic', () => {
-  const { getByTestId } = render(<Login />)
 
   const subheaderEl = getByTestId('subheader')
 
   expect(subheaderEl.textContent).toBe('Videologic presents...')
 })
 
-test("sign in function should make popup", () => {
-  expect()
+test("sign in function should fire once on click", () => {
+  const button = getByTestId('Button')
+  const signInWithFirebase = jest.fn()
+
+  render(<button onClick={signInWithFirebase()}/>)
+  fireEvent.click(screen.getByText(/Login with Google/i))
+  expect(signInWithFirebase).toHaveBeenCalledTimes(1)
+})
+
+test("login container has correct class name", () => {
+  const loginContainer = getByTestId('Login')
+
+  expect(loginContainer.className).toBe("login-container");
 })
